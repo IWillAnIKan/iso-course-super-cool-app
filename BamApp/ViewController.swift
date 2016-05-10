638fbcb5d792b9e93a109a506e5755cf5674dcdc
William Anikin <williamanikin@williams-macbook-pro.local>
2016-05-10 01:03:48 -0400

    Initial Commit

---
+++ BamApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BamApp
+//
+//  Created by William Anikin on 2016-05-10.
+//  Copyright © 2016 William Anikin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
